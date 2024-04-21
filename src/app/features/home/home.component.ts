550a1f9f60462589508f8d73fb0b0b4267f77174
noemivadala <noemi.vadala.nv@gmail.com>
2024-04-21 16:01:16 +0200

    modifiche css home + aggiunta jquery

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -10,34 +10,58 @@ import { v4 as uuidv4 } from 'uuid';
   standalone: true,
   imports: [CommonModule, FormsModule],
   template: `
-    <button>Aggiungi contatto</button>
-    <form (ngSubmit)="addUser(newUserForm)" #newUserForm="ngForm">
-      <input type="text" name="name" placeholder="Nome" ngModel required>
-      <input type="text" name="username" placeholder="Cognome" ngModel required>
-      <input type="email" name="email" placeholder="Email" ngModel required>
-      <input type="tel" name="phone" placeholder="Telefono" ngModel required>
-      <button type="submit">Aggiungi contatto</button>
-    </form>
+    <div class="container-full mb-5">
+      <nav class="navbar navbar-light bg-light">
+        <div class="container-fluid d-flex justify-content-center">
+          <span class="navbar-brand mb-0 h1 text-center">Gestione Contatti</span>
+        </div>
+      </nav>
+    </div>
+    <div class="container mb-5">
+      <h2>📑 Lista contatti</h2>
+      <button>Aggiungi contatto</button>
+      <form (ngSubmit)="addUser(newUserForm)" #newUserForm="ngForm">
+        <input type="text" name="name" placeholder="Nome" ngModel required>
+        <input type="text" name="username" placeholder="Cognome" ngModel required>
+        <input type="email" name="email" placeholder="Email" ngModel required>
+        <input type="tel" name="phone" placeholder="Telefono" ngModel required>
+        <button type="submit">Aggiungi contatto</button>
+      </form>
+      <div class="container container-card d-inline-flex">
+        <div class="card d-flex justify-content-between" *ngFor="let user of users">
+          <div class="detail-user">
+            <p>Nome: {{ user.name }}</p>
+            <p>Username: {{ user.username }}</p>
+            <p>Email: {{ user.email }}</p>
+            <p>Telefono: {{ user.phone }}</p>
+          </div>
+          <div class="btn-user">
+            <button class="btn-edit" (click)="editUser(user)"><i class="fa-regular fa-pen-to-square fa-sm"></i></button>
+            <button class="btn-delete" (click)="deleteUser(user.id)"><i class="fa-regular fa-trash-can fa-sm"></i></button>
+          </div>
+        </div>
+      </div>
+      <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">Toggle right offcanvas</button>
 
-    <h2>Elenco contatti</h2>
-    <ul>
-      <li *ngFor="let user of users">
-        <p>Nome: {{ user.name }}</p>
-        <p>Username: {{ user.username }}</p>
-        <p>Email: {{ user.email }}</p>
-        <p>Telefono: {{ user.phone }}</p>
-        <button (click)="deleteUser(user.id)">Elimina</button>
-        <button (click)="editUser(user)">Modifica</button>
-      </li>
-    </ul>
+      <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+        <div class="offcanvas-header">
+          <h5 id="offcanvasRightLabel">Offcanvas right</h5>
+          <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body">
+          ...
+        </div>
+      </div>
 
-    <form *ngIf="userToUpdate" (ngSubmit)="editUserForm(editUserValue, userToUpdate)" #editUserValue="ngForm">
-      <input type="text" name="name" placeholder="Nome" ngModel required>
-      <input type="text" name="username" placeholder="Cognome" ngModel required>
-      <input type="email" name="email" placeholder="Email" ngModel required>
-      <input type="tel" name="phone" placeholder="Telefono" ngModel required>
-      <button type="submit">Modifica contatto</button>
-    </form>
+      <form *ngIf="userToUpdate" (ngSubmit)="editUserForm(editUserValue, userToUpdate)" #editUserValue="ngForm">
+        <input type="text" name="name" placeholder="Nome" ngModel required>
+        <input type="text" name="username" placeholder="Cognome" ngModel required>
+        <input type="email" name="email" placeholder="Email" ngModel required>
+        <input type="tel" name="phone" placeholder="Telefono" ngModel required>
+        <button type="submit">Modifica contatto</button>
+      </form>
+    </div>
+    
   `,
   styles: ``
 })
